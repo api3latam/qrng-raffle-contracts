0468f010b1a7e90c89e77483547d99ef762c6d73
gpablo6 <gpablo6@outlook.com>
2022-09-28 10:35:50 -0600

    Update batch request. WIP Batch mint

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -18,14 +18,15 @@ contract NFT is ERC721, RrpRequesterV0, Ownable {
     using Strings for uint256;
     using Counters for Counters.Counter;
 
-    bool private shinnyAvailable;       // Turns on and off the possibility to get a special token
-    uint256 private shinnyCount;        // Count for the number of special tokens given
-    uint256 private expectedShinny;     // Ammount of expect special tokens to be minted
-    Counters.Counter private index;	    // Track the next TokenId to be minted
-    string private _baseURIextended;    // The Extended baseUrl for ERC721
-    address public airnode;             // The address of the QRNG airnode
-    bytes32 public endpointIdUint256;   // The endpointId of the airnode to fetch a single random number
-    address public sponsorWallet;       // The address of the sponsorWallet that will be making the fullfillment transaction
+    bool private shinnyAvailable;           // Turns on and off the possibility to get a special token
+    uint256 private shinnyCount;            // Count for the number of special tokens given
+    uint256 private expectedShinny;         // Ammount of expect special tokens to be minted
+    Counters.Counter private index;	        // Track the next TokenId to be minted
+    string private _baseURIextended;        // The Extended baseUrl for ERC721
+    address public airnode;                 // The address of the QRNG airnode
+    bytes32 public endpointIdUint256;       // The endpointId of the airnode to fetch a single random number
+    bytes32 public endpointIdUint256Array;  // The endpointId of the airnode to fetch multiple random numbers at once 
+    address public sponsorWallet;           // The address of the sponsorWallet that will be making the fullfillment transaction
 
     // Mapping a custom URI to a tokenId. In cases when not using root folder from baseURI
     mapping(uint256 => string) private _tokenURIs;
@@ -34,11 +35,17 @@ contract NFT is ERC721, RrpRequesterV0, Ownable {
     // Mapping that maps the requestId for a random number to the fullfillment status of that request
     mapping(bytes32 => bool) public expectingRequestWithIdToBeFulfilled;
     // Mapping that tracks the requestId to the address that made the request
-    mapping(bytes32 => address) private requestToSender;
+    mapping(bytes32 => address[]) private requestToSender;
 
     event SetBaseURI(string baseURIExtended);
-    event SetRequestParameters(address airnodeAddress, bytes32 targetEndpoint, address sponsorAddress);
+    event SetRequestParameters(
+        address airnodeAddress, 
+        bytes32 targetEndpoint,
+        bytes32 targetArrayEndpoint,
+        address sponsorAddress
+    );
     event RequestedToken(address requesterAddress, bytes32 idOfRequest);
+    event RequestedBatchToken(address[] requesterAddresses, bytes32 idOfRequest);
     event GeneratedToken(address requesterAddress, uint256 generatedTokenId);
 
     constructor(address _airnodeRrp, uint256 totalShinnies)
@@ -58,12 +65,19 @@ contract NFT is ERC721, RrpRequesterV0, Ownable {
     function setRequestParameters(
         address _airnode,
         bytes32 _endpointIdUint256,
+        bytes32 _endpointIdUint256Array,
         address _sponsorWallet
     ) external onlyOwner {
         airnode = _airnode;
         endpointIdUint256 = _endpointIdUint256;
+        endpointIdUint256Array = _endpointIdUint256Array;
         sponsorWallet = _sponsorWallet;
-        emit SetRequestParameters(airnode, endpointIdUint256, sponsorWallet);
+        emit SetRequestParameters(
+            airnode, 
+            endpointIdUint256Array,
+            endpointIdUint256, 
+            sponsorWallet
+        );
     }
     
     /**
@@ -182,8 +196,32 @@ contract NFT is ERC721, RrpRequesterV0, Ownable {
             ""
         );
         expectingRequestWithIdToBeFulfilled[requestId] = true;
-        requestToSender[requestId] = targetAddress;
+        requestToSender[requestId].push(targetAddress);
         emit RequestedToken(targetAddress, requestId);
+        return requestId;
+    }
+
+    /**
+     * @notice Call the Airnode in batch for the QRNG Services.
+     * @param targetAddresses The addresses that maps to this request.  
+     */
+    function requestBatchToken(
+        address[] memory targetAddresses
+    ) public onlyOwner returns (bytes32) {
+        bytes32 requestId = airnodeRrp.makeFullRequest(
+            airnode,
+            endpointIdUint256Array,
+            address(this),
+            sponsorWallet,
+            address(this),
+            this.mintBatch.selector,
+            abi.encode(bytes32("1u"), bytes32("size"), targetAddresses.length)
+        );
+        expectingRequestWithIdToBeFulfilled[requestId] = true;
+        for (uint256 i=0; i <= targetAddresses.length; i++) {
+            requestToSender[requestId].push(targetAddresses[i]);
+        }
+        emit RequestedBatchToken(targetAddresses, requestId);
         return requestId;
     }
 
@@ -207,13 +245,29 @@ contract NFT is ERC721, RrpRequesterV0, Ownable {
         expectingRequestWithIdToBeFulfilled[requestId] = false;
         uint256 qrngUint256 = abi.decode(data, (uint256));
         uint256 tokenId = _generateId(qrngUint256);
-        _safeMint(requestToSender[requestId], tokenId);
-        if (tokenId < expectedShinny) {
-            _setTokenURI(tokenId, tokenId.toString());
-        } else {
-            _setTokenURI(tokenId, tokenId.toString());
+        _safeMint(requestToSender[requestId][0], tokenId);
+        _setTokenURI(tokenId, tokenId.toString());
+        emit GeneratedToken(requestToSender[requestId][0], tokenId);
+    }
+
+    /**
+     * @notice
+     */
+    function mintBatch(
+        bytes32 requestId,
+        bytes calldata data
+    ) external onlyAirnodeRrp {
+        require(
+            expectingRequestWithIdToBeFulfilled[requestId],
+            "Request ID not known"
+        );
+        expectingRequestWithIdToBeFulfilled[requestId] = false;
+        uint256[] memory qrngUintArray = abi.decode(data, (uint256[]));
+        uint256[] memory tokenIds;
+        for (uint256 i=0; i <= qrngUintArray.length; i++) {
+            tokenIds[i] = _generateId(qrngUintArray[i]);
+            
         }
-        emit GeneratedToken(requestToSender[requestId], tokenId);
     }
 
     /**
