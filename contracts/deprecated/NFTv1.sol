4b2ee732b2f4d017cb628bc25cba4abea6fd35a3
gpablo6 <gpablo6@outlook.com>
2022-10-02 15:47:55 -0600

    New NFT Stable version

--- contracts/deprecated/NFTv1.sol
+++ contracts/deprecated/NFTv1.sol
@@ -13,7 +13,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 * https://gist.github.com/Ashar2shahid/e7b70712f23dd33b556aa66a6d8788b4
 */
 
-contract NFT is ERC721, RrpRequesterV0, Ownable {
+contract NFTv1 is ERC721, RrpRequesterV0, Ownable {
 
     using Strings for uint256;
     using Counters for Counters.Counter;
@@ -25,6 +25,7 @@ contract NFT is ERC721, RrpRequesterV0, Ownable {
     string private _baseURIextended;        // The Extended baseUrl for ERC721
     address public airnode;                 // The address of the QRNG airnode
     bytes32 public endpointIdUint256;       // The endpointId of the airnode to fetch a single random number
+    bytes32 public endpointIdUint256Array;  // The endpointId of the airnode to fetch multiple random numbers at once 
     address private sponsorWallet;           // The address of the sponsorWallet that will be making the fullfillment transaction
 
     // Mapping a custom URI to a tokenId. In cases when not using root folder from baseURI
@@ -38,11 +39,13 @@ contract NFT is ERC721, RrpRequesterV0, Ownable {
     event SetRequestParameters(
         address airnodeAddress, 
         bytes32 targetEndpoint,
+        bytes32 targetArrayEndpoint,
         address sponsorAddress
     );
-    event SwitchShinny(bool shinnyBool);
     event RequestedToken(address requesterAddress, bytes32 idOfRequest);
+    event RequestedBatchToken(address[] requesterAddresses, bytes32 idOfRequest);
     event GeneratedToken(address requesterAddress, uint256 generatedTokenId);
+    event GeneratedBatchToken(address[] requesterAddresses, uint256[] generatedTokenIds);
     event GenerateShinny(address winnerAddress, uint256 generatedTokenId);
 
     constructor(address _airnodeRrp, uint256 totalShinnies)
@@ -62,13 +65,16 @@ contract NFT is ERC721, RrpRequesterV0, Ownable {
     function setRequestParameters(
         address _airnode,
         bytes32 _endpointIdUint256,
+        bytes32 _endpointIdUint256Array,
         address _sponsorWallet
     ) external onlyOwner {
         airnode = _airnode;
         endpointIdUint256 = _endpointIdUint256;
+        endpointIdUint256Array = _endpointIdUint256Array;
         sponsorWallet = _sponsorWallet;
         emit SetRequestParameters(
-            airnode,
+            airnode, 
+            endpointIdUint256Array,
             endpointIdUint256, 
             sponsorWallet
         );
@@ -84,7 +90,6 @@ contract NFT is ERC721, RrpRequesterV0, Ownable {
             } else {
                 shinnyAvailable = true;
             }
-            emit SwitchShinny(shinnyAvailable);
     }
     
     /**
@@ -201,6 +206,30 @@ contract NFT is ERC721, RrpRequesterV0, Ownable {
     }
 
     /**
+     * @notice Call the Airnode in batch for the QRNG Services.
+     * @param targetAddresses The addresses that maps to this request.  
+     */
+    function requestBatchToken(
+        address[] memory targetAddresses
+    ) public onlyOwner returns (bytes32) {
+        bytes32 requestId = airnodeRrp.makeFullRequest(
+            airnode,
+            endpointIdUint256Array,
+            address(this),
+            sponsorWallet,
+            address(this),
+            this.mintBatch.selector,
+            abi.encode(bytes32("1u"), bytes32("size"), targetAddresses.length)
+        );
+        expectingRequestWithIdToBeFulfilled[requestId] = true;
+        for (uint256 i=0; i <= targetAddresses.length; i++) {
+            requestToSender[requestId].push(targetAddresses[i]);
+        }
+        emit RequestedBatchToken(targetAddresses, requestId);
+        return requestId;
+    }
+
+    /**
      * @notice Callback Function for the Airnode, which recieves the Random Id and mints the NFT.
      * @dev Notice the `onlyAirnodeRrp` modifier. You should only accept RRP
      * fulfillments from this protocol contract. Also note that only
@@ -223,6 +252,31 @@ contract NFT is ERC721, RrpRequesterV0, Ownable {
         _safeMint(requestToSender[requestId][0], tokenId);
         _setTokenURI(tokenId, tokenId.toString());
         emit GeneratedToken(requestToSender[requestId][0], tokenId);
+    }
+
+    /**
+     * @notice Callback Function for airnode Batch Request.
+     * @param requestId Request ID.
+     * @param data ABI-encoded response. The Random Number from the API.
+     */
+    function mintBatch(
+        bytes32 requestId,
+        bytes calldata data
+    ) external onlyAirnodeRrp {
+        require(
+            expectingRequestWithIdToBeFulfilled[requestId],
+            "Request ID not known"
+        );
+        expectingRequestWithIdToBeFulfilled[requestId] = false;
+        uint256[] memory qrngUintArray = abi.decode(data, (uint256[]));
+        uint256[] memory tokenIds;
+        for (uint256 i=0; i <= qrngUintArray.length; i++) {
+            uint256 generatedToken =  _generateId(qrngUintArray[i], false);
+            tokenIds[i]= generatedToken;
+            _safeMint(requestToSender[requestId][i], generatedToken);
+            _setTokenURI(generatedToken, generatedToken.toString());
+        }
+        emit GeneratedBatchToken(requestToSender[requestId], tokenIds);
     }
 
     /**
