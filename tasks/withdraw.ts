0b96a4b0a633db9f11a5e8926b21c292ae0a4fe2
gpablo3 <giancarlo@api3.org>
2022-11-02 12:17:00 -0600

    Add workflow for withdrawing from spooky

--- tasks/withdraw.ts
+++ tasks/withdraw.ts
@@ -1,9 +1,11 @@
 import { task } from "hardhat/config";
 import { AirnodeRrpAddresses, AirnodeRrpV0, AirnodeRrpV0Factory } from '@api3/airnode-protocol';
 import { requestWithdrawal, 
-    deriveSponsorWalletAddress } from "@api3/airnode-admin";
+    deriveSponsorWalletAddress,
+    unsponsorRequester } from "@api3/airnode-admin";
 import { providerURL,
-        loadJsonFile } from "../scripts/utils";
+        loadJsonFile, 
+        getPrivateKey } from "../scripts/utils/misc";
 
 task("withdraw", "Set a request to withdraw funds from airnode")
     .setAction(async (_, hre) => {
@@ -12,24 +14,35 @@ task("withdraw", "Set a request to withdraw funds from airnode")
             providerURL(hre.network.name)
         );
 
-        const airnode = AirnodeRrpV0Factory.getContract(
+        const airnodeContract = AirnodeRrpV0Factory.getContract(
             AirnodeRrpAddresses[hre.network.config.chainId as number],
             AirnodeRrpV0Factory.abi
-        ) as AirnodeRrpV0
+        ) as AirnodeRrpV0;
 
-        const nftAddress = loadJsonFile(`addresses/nft${hre.network.name}.json`)['nft'];
+        const signer = new hre.ethers.Wallet(getPrivateKey(), provider);
+        const spookyAddress = loadJsonFile(`addresses/spooky${hre.network.name}.json`)['spooky'];
         
         const sponsorWallet = await deriveSponsorWalletAddress(
             qrng['xpub'],
             qrng['airnode'],
-            nftAddress
-        )
+            signer.address
+        );
         
+        const airnode = airnodeContract.connect(signer)
+
         console.log('Requesting withdrawal');
-        const result = await requestWithdrawal(
+        await requestWithdrawal(
             airnode,
             qrng['airnode'],
             sponsorWallet
         );
-        console.log(`Done with the following result: ${result}`);
+        console.log('Done requesting withdrawal!\
+        \nUnsponsoring requester contract');
+
+        await unsponsorRequester(
+            airnode,
+            spookyAddress
+        );
+        console.log(`Done unsponsoring contract with address: \
+        ${spookyAddress}`);
     });
